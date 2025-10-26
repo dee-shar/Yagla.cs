9821ad2bc7f561dfef482511ded3e88c45fa3493
huok <77536370+dee-shar@users.noreply.github.com>
2025-10-26 03:52:40 +0300

    Update and rename minute_inbox.sh to Yagla.cs

---
+++ src/Yagla.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Net.Http.Headers;
+
+namespace YaglaApi
+{
+    public class Yagla
+    {
+        private readonly HttpClient httpClient;
+        private readonly string apiUrl = "https://yagla.ru/tools";
+        public Yagla()
+        {
+            httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.UserAgent.ParseAdd(
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36");
+            httpClient.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        public async Task<string> GenerateShortLink(string link)
+        {
+            var data = JsonContent.Create(new { link = link });
+            var response = await httpClient.PostAsync($"{apiUrl}/generateShortLink", data);
+            return await response.Content.ReadAsStringAsync();
+        }
+    }
+}
